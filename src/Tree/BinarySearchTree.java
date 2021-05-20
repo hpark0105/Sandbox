78839311c9cddd5056606fbd1a557621285caa82
Hung Park <hpark0105@gmail.com>
2021-05-20 09:49:56 -0500

    init tree

--- src/Tree/BinarySearchTree.java
+++ src/Tree/BinarySearchTree.java
@@ -1,5 +1,18 @@
 package Tree;
 
 public class BinarySearchTree {
+    //inner Node class
+    class Node {
+        int data;
+        Node root;
+        Node() {
+            data = 0;
+            root = new Node();
+        }
+    }
+
+    public static void main(String args[]){
+        System.out.println("tree example");
+    }
 
 }
